c2ae7b53c15e2d32748e7b36fc40163b641e8ec3
tommyk257 <tommyk257@gmail.com>
2023-08-31 02:28:51 +0100

    add font to app.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,61 @@
 import "./App.css";
 import Header from "./components/Header";
+import Basket from "./components/Basket";
 import Mainsec from "./components/Mainsec";
 import SingleProductCom from "./components/SingleProductCom";
-import Products from "./pages/Products"
-import VegProducts from "./pages/VegProducts"
-import MeatProducts from "./pages/MeatsProducts"
-import DairyProducts from "./pages/DairyProducts"
+import Products from "./pages/Products";
+import VegProducts from "./pages/VegProducts";
+import MeatProducts from "./pages/MeatsProducts";
+import DairyProducts from "./pages/DairyProducts";
 import { Route, Routes } from "react-router-dom";
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { Helmet } from "react-helmet";
 
-
-function App(props) {
+function App() {
   const [basket, setBasket] = useState([]);
+  
 
   const addProductToBasket = (product) => {
     setBasket([...basket, product]);
   };
 
+
   return (
-    
     <div className="App">
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@900&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
       <Header basket={basket} />
-        <Routes>
-          <Route path="/" element={<Mainsec />} />
-          <Route path="/Products" element={<Products addProductToBasket={addProductToBasket} />} />
-          <Route path="/product/:productName" element={<SingleProductCom addProductToBasket={addProductToBasket} />}/>
-          <Route path="/Vegs" element={<VegProducts addProductToBasket={addProductToBasket} />} />
-          <Route path="/Meats" element={<MeatProducts addProductToBasket={addProductToBasket} />} />
-          <Route path="/Dairies" element={<DairyProducts addProductToBasket={addProductToBasket} />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Mainsec />} />
+        <Route
+          path="/Products"
+          element={<Products addProductToBasket={addProductToBasket} />}
+        />
+        <Route
+          path="/product/:productName"
+          element={<SingleProductCom addProductToBasket={addProductToBasket} />}
+        />
+        <Route
+          path="/Vegs"
+          element={<VegProducts addProductToBasket={addProductToBasket} />}
+        />
+        <Route
+          path="/Meats"
+          element={<MeatProducts addProductToBasket={addProductToBasket} />}
+        />
+        <Route
+          path="/Dairies"
+          element={<DairyProducts addProductToBasket={addProductToBasket} />}
+        />
+      </Routes>
       <footer></footer>
     </div>
-    
   );
 }
 
