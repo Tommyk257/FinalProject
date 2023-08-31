1814ba7568b16bb049f3aeb0483d5a4c228dc050
tommyk257 <tommyk257@gmail.com>
2023-08-31 02:28:25 +0100

    css and basket changes

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React from "react";
+
 
 function Basket({ basket }) {
  
 
-    return (
-        <div className="basket-box-grid">
-        <h1>basket</h1>
-        <section className="product-list">
+
+  return (
+    <div className="basket-box-grid">
+      <h1>basket</h1>
+      <section className="product-list">
         <ul>
           {basket.map((item) => (
-            <li key={item.id}>
-              <div>{item.product_name} {item.product_price}</div>
+            <li key={item.id} className="basket-item">
+              <img
+                className="basket-image"
+                src={item.product_image}
+                alt={item.product_name}
+              />
+              <div className="basket-details">
+                <div className="basket-name">{item.product_name}</div>
+              </div>
+              <div className="basket-price">£{item.product_price}</div>
+              <button></button>
             </li>
           ))}
         </ul>
-        </section>
-      </div>
-    );
-  }
-  
-  export default Basket;
-  
+      </section>
+    </div>
+  );
+}
+
+export default Basket;
