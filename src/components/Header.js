5f9689980273d2cbf0715a278d47b8c1a79a92c5
tommyk257 <tommyk257@gmail.com>
2023-08-31 02:29:05 +0100

    code tidy on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import "react-sliding-pane/dist/react-sliding-pane.css";
-import { Helmet } from "react-helmet";
+
 import Basket from "../components/Basket";
 import SlidingPane from "react-sliding-pane";
 import { basketIcon } from "../icons/basket.js";
 import Logo from "./Logo";
 import SearchBar from "./SearchBar";
-import DropdownMenu from './DropDownMenu.js';
-import PromotionsMenu from './PromotionsMenu.js';
-import RecipesMenu from './RecipesMenu.js';
+import DropdownMenu from "./DropDownMenu.js";
+import PromotionsMenu from "./PromotionsMenu.js";
+import RecipesMenu from "./RecipesMenu.js";
 
 function Header({ basket }) {
   const [isPaneOpen, setIsPaneOpen] = useState(false);
@@ -17,23 +17,22 @@ function Header({ basket }) {
   // droppDownMenu
 
   const productsMenuItems = [
-    { label: 'Fruit/Veg', link: '/Vegs' },
-    { label: 'Meats',     link: '/Meats' },
-    { label: 'Dairy',     link: '/Dairies' },
+    { label: "Fruit/Veg", link: "/Vegs" },
+    { label: "Meats", link: "/Meats" },
+    { label: "Dairy", link: "/Dairies" },
   ];
 
   const promotionMenu = [
-    { label: 'Offers', link: '/category1' },
-    { label: 'BBQ Special', link: '/category2' },
-    { label: 'Back to school', link: '/category3' },
+    { label: "Offers", link: "/category1" },
+    { label: "BBQ Special", link: "/category2" },
+    { label: "Back to school", link: "/category3" },
   ];
 
   const recipesMenu = [
-    { label: 'Category 1', link: '/category1' },
-    { label: 'Category 2', link: '/category2' },
-    { label: 'Category 3', link: '/category3' },
+    { label: "Category 1", link: "/category1" },
+    { label: "Category 2", link: "/category2" },
+    { label: "Category 3", link: "/category3" },
   ];
-
 
   // bassket pannel
 
@@ -58,14 +57,6 @@ function Header({ basket }) {
 
   return (
     <>
-      <Helmet>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@900&display=swap"
-          rel="stylesheet"
-        />
-      </Helmet>
       <div className="header">
         <div className="head-nav-bar">
           <Logo />
